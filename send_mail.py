6514b40f3966adbb343ac2e41e862dca3c267408
Siddhant Baliram Fulzele <31157081+siddfulzele@users.noreply.github.com>
2020-12-31 23:59:25 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -3,24 +3,24 @@ import smtplib
 
 link ="https://forms.gle/aikahxBhaTV6QHPFA"
 image = "dostipine_projectelevation.jpg"
-filepath = "C:/Users/Sahil/Desktop/project/email/sendinblue/1st_300.csv"
-sender = 'raju613691@gmail.com'
+filepath = "C:/Users/Sidd/Desktop/project/email/sendinblue/1st_300.csv"
+sender = 'siddfulzele@gmail.com'
 emails = pd.read_csv(filepath,header=None)
 emails = list(emails[0])
 
-message = """From: Sahil Kapoor <from@fromdomain.com>
-To: Sahan <to@todomain.com>
+message = """From: Siddhant Fulzele <from@fromdomain.com>
+To: Person <to@todomain.com>
 MIME-Version: 1.0
 Content-type: text/html
 Subject: Dosti Pine Project
 
 <center>
 <a href="""+link+""">
-<img src="https://raw.githubusercontent.com/Sahan3/Image/master/"""+image+"""" alt="This IS Image1">
+<img src="https://raw.githubusercontent.com/siddfulzele/Temp/master/"""+image+"""" alt="This IS Image1">
 <br>
 <img style="width: 100%;
   max-width: 800px;
-  height: auto;" src="https://raw.githubusercontent.com/Sahan3/Image/master/dosti_pine.jpg" alt="This IS Image">
+  height: auto;" src="https://raw.githubusercontent.com/siddfulzele/Temp/master/dosti_pine.jpg" alt="This IS Image">
 </a>
 <br>
 <a href="""+link+""" ><button style="background-color: #4CAF50;
@@ -44,8 +44,8 @@ for email in emails:
       smtpObj = smtplib.SMTP('smtp.gmail.com:587')
       smtpObj.ehlo()
       smtpObj.starttls()
-      smtpObj.login("raju613691@gmail.com","Sahil8080603473")
-      smtpObj.sendmail("raju613691@gmail.com", email, message)
+      smtpObj.login(sender,"***password***")
+      smtpObj.sendmail(sender, email, message)
       smtpObj.quit()
 
       print("Successfully sent email")
